47e076fb7fe5c9a25b7e38bfa9bf5205af661be8
Radhika Yusuf <radhika.yusuf1@gmail.com>
2017-07-27 12:42:33 +0700

    implement logic for onSaveInstanceState/onRestoreInstanceState

--- app/src/main/java/radhika/yusuf/id/mymovie/utils/Constant.java
+++ app/src/main/java/radhika/yusuf/id/mymovie/utils/Constant.java
@@ -5,16 +5,9 @@ package radhika.yusuf.id.mymovie.utils;
  */
 
 public class Constant {
-    public static final String API_KEY = "983e2812b719e865d53ccf59c73d7624";
+    public static final String API_KEY = "######################";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/w185/";
-    public static final String KEY_SERVICE = "AIzaSyBIrnKbPS4TkjKg6UBPStt4keF5olTT5oQ";
+    public static final String KEY_SERVICE = "AIza###########################3";
 
-    /**
-     *
-     public static final String API_KEY = "983e2812b719e865d53ccf59c73d7624";
-     public static final String BASE_URL = "https://api.themoviedb.org/3/";
-     public static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/w185/";
-     public static final String KEY_SERVICE = "AIzaSyBIrnKbPS4TkjKg6UBPStt4keF5olTT5oQ";
-     */
 }
